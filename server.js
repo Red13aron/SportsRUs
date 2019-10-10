9c6d6dd6a383b989670587ecd96d8ecba2741b34
SNelson <snelson1337@gmail.com>
2019-10-10 14:49:53 -0500

    Added uri for deployment db

--- server.js
+++ server.js
@@ -27,7 +27,8 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/sportsRUs", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 // Routes
 
